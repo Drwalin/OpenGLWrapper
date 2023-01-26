d455bdda7b7f25ba9e844e7336aef374dae2ee3e
Drwalin <34726800+Drwalin@users.noreply.github.com>
2023-01-27 00:57:34 +0100

    Added some more AssimpLoader code.

--- src/AssimpLoader.cpp
+++ src/AssimpLoader.cpp
@@ -18,8 +18,6 @@
 
 #include <cstdio>
 
-#define DEBUG {printf(" :%i\n", __LINE__); fflush(stdout);}
-
 #include <openglwrapper/AssimpLoader.h>
 
 #include <assimp/Importer.hpp>
@@ -27,13 +25,123 @@
 #include <assimp/postprocess.h>
 
 namespace gl {
+	void AssimpLoader::Mesh::LoadMesh(const aiMesh* mesh) {
+		uint32_t vertices = mesh->mNumVertices;
+		uint32_t colors = mesh->GetNumColorChannels();
+		uint32_t uvs = mesh->GetNumUVChannels();
+		
+		
+		
+		
+		
+		if(mesh->HasPositions()) {
+			pos.resize(1);
+			pos[0].resize(vertices);
+			for(int i=0; i<mesh->mNumVertices; ++i) {
+				pos[0][i] = {
+						mesh->mVertices[i].x,
+						mesh->mVertices[i].y,
+						mesh->mVertices[i].z
+				};
+			}
+		}
+		
+		if(mesh->HasVertexColors(0)) {
+			color.resize(colors);
+			for(int j=0; j<colors; ++j) {
+				color[j].resize(vertices);
+				for(int i=0; i<mesh->mNumVertices; ++i) {
+					color[j][i] = {
+							mesh->mColors[j][i].r,
+							mesh->mColors[j][i].g,
+							mesh->mColors[j][i].b,
+							mesh->mColors[j][i].a
+					};
+				}
+			}
+		}
+		
+		if(mesh->GetNumUVChannels() > 0) {
+			uv.resize(uvs);
+			for(int j=0; j<uvs; ++j) {
+				uv[j].resize(vertices);
+				for(int i=0; i<mesh->mNumVertices; ++i) {
+					uv[j][i] = {
+							mesh->mTextureCoords[j][i].x,
+							mesh->mTextureCoords[j][i].y
+					};
+				}
+			}
+		}
+		
+		if(mesh->HasNormals()) {
+			normal.resize(1);
+			normal[0].resize(vertices);
+			for(int i=0; i<mesh->mNumVertices; ++i) {
+				normal[0][i] = {
+						mesh->mNormals[i].x,
+						mesh->mNormals[i].y,
+						mesh->mNormals[i].z
+				};
+			}
+		}
+		
+		if(mesh->HasBones()) {
+			const int weightsPerVert = 4;
+			weight.resize(weightsPerVert);
+			weightBone.resize(weightsPerVert);
+			for(int i=0; i<weightsPerVert; ++i) {
+				weight[i].clear();
+				weight[i].resize(vertices, {0});
+				weightBone[i].clear();
+				weightBone[i].resize(vertices, {0});
+			}
+			for(int b=0; b<mesh->mNumBones; ++b) {
+				aiBone* bone = mesh->mBones[b];
+				for(int i=0; i<bone->mNumWeights; ++i) {
+					aiVertexWeight W = bone->mWeights[i];
+					uint32_t v = W.mVertexId;
+					float w = W.mWeight;
+					uint32_t minId = 3;
+					float min = weight[3][v].v[0];
+					for(int j=3; j>=0; --j) {
+						if(min >= weight[j][v].v[0]) {
+							min = weight[j][v].v[0];
+							minId = j;
+						}
+					}
+					if(min < w) {
+						weight[minId][v].v[0] = w;
+						weightBone[minId][v].v[0] = b;
+					}
+				}
+			}
+		}
+		
+		for(int i=0; i<mesh->mNumFaces; ++i) {
+			auto f = mesh->mFaces[i];
+			for(int j=2; j<f.mNumIndices; ++j) {
+				indices.emplace_back(f.mIndices[0]);
+				indices.emplace_back(f.mIndices[j-1]);
+				indices.emplace_back(f.mIndices[j]);
+			}
+		}
+		
+		this->name = mesh->mName.C_Str();
+	}
+	
 	void AssimpLoader::Load(const char* file) {
 		Assimp::Importer importer;
-		auto s = importer.ReadFile(file, aiProcess_Triangulate | aiProcess_JoinIdenticalVertices);
-// 		int numMesh = s->mNumMeshes;
+		const ::aiScene* s = importer.ReadFile(file, aiProcess_Triangulate | aiProcess_JoinIdenticalVertices);
 		auto m = s->mMeshes[0];
 		
-		printf(" num meshes = %i\n", s->mNumMeshes);
+		this->meshes.clear();
+		this->meshes.resize(s->mNumMeshes);
+		for(int i=0; i<s->mNumMeshes; ++i) {
+			meshes[i].LoadMesh(s->mMeshes[i]);
+		}
+		
+		
 		
 		pos.resize(m->mNumVertices);
 		color.resize(m->mNumVertices);
@@ -41,12 +149,8 @@ namespace gl {
 		norm.resize(m->mNumVertices);
 		indices.reserve(m->mNumFaces*3);
 		
-		DEBUG;
 		if(m->HasPositions()) {
-		DEBUG;
 			for(int i=0; i<m->mNumVertices; ++i) {
-			if(!i)
-		DEBUG;
 				pos[i] = glm::vec3(
 						m->mVertices[i].x,
 						m->mVertices[i].y,
@@ -55,12 +159,8 @@ namespace gl {
 			}
 		}
 		
-		DEBUG;
 		if(m->HasVertexColors(0)) {
-		DEBUG;
 			for(int i=0; i<m->mNumVertices; ++i) {
-			if(!i)
-		DEBUG;
 				color[i] = glm::vec4(
 						m->mColors[0][i].r,
 						m->mColors[0][i].g,
@@ -70,12 +170,8 @@ namespace gl {
 			}
 		}
 		
-		DEBUG;
 		if(m->GetNumUVChannels() > 0) {
-		DEBUG;
 			for(int i=0; i<m->mNumVertices; ++i) {
-			if(!i)
-		DEBUG;
 				uv[i] = glm::vec2(
 						m->mTextureCoords[0][i].x,
 						m->mTextureCoords[0][i].y
@@ -83,12 +179,8 @@ namespace gl {
 			}
 		}
 		
-		DEBUG;
 		if(m->HasNormals()) {
-		DEBUG;
 			for(int i=0; i<m->mNumVertices; ++i) {
-			if(!i)
-		DEBUG;
 				norm[i] = glm::vec3(
 						m->mNormals[i].x,
 						m->mNormals[i].y,
@@ -97,20 +189,22 @@ namespace gl {
 			}
 		}
 		
-		DEBUG;
 		for(int i=0; i<m->mNumFaces; ++i) {
-			if(!i)
-		DEBUG;
 			auto f = m->mFaces[i];
 			for(int j=2; j<f.mNumIndices; ++j) {
-// 			int j = 2;
-			if(!i)
-		DEBUG;
 				indices.emplace_back(f.mIndices[0]);
 				indices.emplace_back(f.mIndices[j-1]);
 				indices.emplace_back(f.mIndices[j]);
 			}
 		}
+		
+		// weights of vertices of bone [0]
+		s->mSkeletons[0]->mBones[0]->mWeights;
+		
+		// animations data probrably per bone
+		s->mAnimations[0]->mChannels[0]->mPositionKeys;
+		s->mAnimations[0]->mChannels[0]->mRotationKeys;
+		s->mAnimations[0]->mChannels[0]->mScalingKeys;
 	}
 } // namespace gl
 
