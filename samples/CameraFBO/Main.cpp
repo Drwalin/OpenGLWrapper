5795311e2e6911d3ff0ccee7121736f46c54d1c7
Drwalin <marek.zalewski.drwalin@gmail.com>
2023-02-18 20:54:21 +0100

    Cleared CameraFBO example.

--- samples/CameraFBO/Main.cpp
+++ samples/CameraFBO/Main.cpp
@@ -30,11 +30,6 @@ int main() {
 			"../../samples/CameraFBO/vertex.glsl",
 			NULL,
 			"../../samples/CameraFBO/fragment.glsl");
-	std::shared_ptr<gl::Shader> shader2 = std::make_shared<gl::Shader>();
-	shader2->Load(
-			"../../samples/CameraFBO/vertex.glsl",
-			NULL,
-			"../../samples/CameraFBO/fragment.glsl");
 	
 	gl::BasicMeshLoader::StaticMeshRenderable
 		terrain("../../samples/Terrain.fbx", 0, shader),
@@ -48,12 +43,6 @@ int main() {
 	int lightDirLoc = shader->GetUniformLocation("lightDir");
 	int useTexLoc = shader->GetUniformLocation("useTex");
 	int texLoc = shader->GetUniformLocation("tex");
-	
-			printf("texLoc = %i\n", texLoc);
-			printf("useTexLoc = %i\n", useTexLoc);
-			printf("viewLoc = %i\n", viewLoc);
-			printf("lightDirLoc = %i\n", lightDirLoc);
-			printf("projLoc = %i\n", projLoc);
 	
 	Camera camera2(glm::vec3(0.0f,0.0f, 0.0f));
 	
@@ -88,53 +77,33 @@ int main() {
 	const uint32_t RENDER_SIZE = 256;
 	
 	// gen and setup FBO
-	std::shared_ptr<gl::Texture> renderTargetTexture
-		= std::make_shared<gl::Texture>();
-	renderTargetTexture->UpdateTextureData(NULL, RENDER_SIZE, RENDER_SIZE,
+	gl::Texture renderTargetTexture;
+	renderTargetTexture.UpdateTextureData(NULL, RENDER_SIZE, RENDER_SIZE,
 			false, gl::TEXTURE_2D, gl::RGBA, gl::RGBA, gl::UNSIGNED_BYTE);
-	renderTargetTexture->WrapX(gl::CLAMP_TO_BORDER);
-	renderTargetTexture->WrapY(gl::CLAMP_TO_BORDER);
-	renderTargetTexture->MinFilter(gl::NEAREST);
-	renderTargetTexture->MagFilter(gl::MAG_NEAREST);
-	
-// 	renderTargetTexture->InitTextureEmpty(RENDER_SIZE, RENDER_SIZE,
-// 			gl::TextureTarget::TEXTURE_2D,
-// 			gl::TextureSizedInternalFormat::RGBA8UI);
-	
-// 	std::shared_ptr<gl::Texture> renderTargetDepth
-// 		= std::make_shared<gl::Texture>();
-// 	renderTargetDepth->UpdateTextureData(NULL, RENDER_SIZE, RENDER_SIZE,
-// 			false, gl::TEXTURE_2D, gl::RGBA, gl::R32, gl::UNSIGNED_INT);
-// 	renderTargetTexture->InitTextureEmpty(RENDER_SIZE, RENDER_SIZE,
-// 			gl::TextureTarget::TEXTURE_2D,
-// 			gl::TextureSizedInternalFormat::RGBA8UI);
+	renderTargetTexture.WrapX(gl::CLAMP_TO_BORDER);
+	renderTargetTexture.WrapY(gl::CLAMP_TO_BORDER);
+	renderTargetTexture.MinFilter(gl::NEAREST);
+	renderTargetTexture.MagFilter(gl::MAG_NEAREST);
 	
 	unsigned int fbo;
 	glGenFramebuffers(1, &fbo);
 	glBindFramebuffer(GL_FRAMEBUFFER, fbo); 
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo); 
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
-			renderTargetTexture->GetTexture(), 0);
-// 	std::shared_ptr<gl::Texture> renderTargetDepth = std::make_shared<gl::Texture>();
-// 	renderTargetDepth->UpdateTextureData(NULL, RENDER_SIZE, RENDER_SIZE, false, gl::TEXTURE_2D,
-// 			(gl::TextureDataFormat)GL_DEPTH24_STENCIL8,
-// 			(gl::TextureDataFormat)GL_DEPTH_STENCIL,
-// 			(gl::DataType)GL_UNSIGNED_INT_24_8);
-// 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, renderTargetDepth->GetTexture(), 0);
-	unsigned int rbo;
-	glGenRenderbuffers(1, &rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, rbo); 
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, RENDER_SIZE, RENDER_SIZE);
-	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+			renderTargetTexture.GetTexture(), 0);
+	
+	std::shared_ptr<gl::Texture> renderTargetDepth = std::make_shared<gl::Texture>();
+	renderTargetDepth->UpdateTextureData(NULL, RENDER_SIZE, RENDER_SIZE, false, gl::TEXTURE_2D,
+			(gl::TextureDataFormat)GL_DEPTH24_STENCIL8,
+			(gl::TextureDataFormat)GL_DEPTH_STENCIL,
+			(gl::DataType)GL_UNSIGNED_INT_24_8);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, renderTargetDepth->GetTexture(), 0);
 	
 	GLenum Status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 
 	if(Status != GL_FRAMEBUFFER_COMPLETE) {
 		printf("FB error, status: 0x%x\n", Status);
 		return false;
-	} else {
-		printf("No FB error\n");
 	}
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	
@@ -153,22 +122,15 @@ int main() {
 						glm::vec3(0,1,0)
 					) * glm::vec4(-1,-0.3,-1,0)
 				));
-		shader2->SetVec4(lightDirLoc,
-				glm::normalize(
-					glm::rotate(
-						glm::mat4(1),
-						(lastFrame)/1.0f,
-						glm::vec3(0,1,0)
-					) * glm::vec4(-1,-0.3,-1,0)
-				));
 		
 		// Render everything for secondary camera (FBO)
 		{
 			shader->Use();
 			// Set up FBO
 			glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-// 			shader->SetTexture(texLoc, NULL, 0);
+			shader->SetTexture(texLoc, NULL, 0);
 			
+			// Set Viewport
 			glViewport(0, 0, RENDER_SIZE, RENDER_SIZE);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			
@@ -201,7 +163,7 @@ int main() {
 			// Render FBO result
 			gl::openGL.FaceCulling(true, true);
 			shader->SetBool(useTexLoc, true);
-			shader->SetTexture(texLoc, renderTargetTexture.get(), 0);
+			shader->SetTexture(texLoc, &renderTargetTexture, 0);
 			shader->SetMat4(modelLoc,
 					glm::scale(
 						glm::translate(
